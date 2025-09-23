639506a231f67f4f8796a75a8998c1a06f4c076f
Flayme <flayme@fedora>
2025-09-23 20:48:07 +0300

    add: Home Task #1
    При виконанні домашнього завдання було створено проєкт на Node.js з підтримкою TypeScript
    - Налаштовано tsconfig.json
    - Створена програма, яка оголошує змінні та зберігає інформацію про користовуча.

---
+++ ts-new-project/src/index.ts
@@ -0,0 +1,28 @@
+type UserInfo = {
+    name: string;
+    birthdayDate: Date;
+    numberPhone: number;
+    address: {
+        street: string;
+        city: string;
+        country: string;
+    };
+    children?: null;
+};
+
+function getMyInfo(): string {
+    const name = "Dmytro";
+    const birthdayDate = new Date("2003-05-13 12:00:00");
+    const numberPhone = "(380) 98-24-72-815";
+    const address = {
+        street: "Chervonoi Kalyny 44",
+        city: "Lviv",
+        state: "Lvivska",
+        zip: "79000",
+        country: "Ukraine"
+    };
+
+    return `Name: ${name}\nBirthday: ${birthdayDate}\nPhone: ${numberPhone}\nAddress: ${JSON.stringify(address)}`;
+}
+
+console.log(getMyInfo());
