42adb4f35ab2453d8af64d08f5167b6b1792f797
Dmytro <166822467+theflayme@users.noreply.github.com>
2025-09-25 19:30:03 +0300

    refresh: Type UserInfo

--- ts-new-project/src/index.ts
+++ ts-new-project/src/index.ts
@@ -3,26 +3,29 @@ type UserInfo = {
     birthdayDate: Date | string;
     numberPhone: number | string;
     address: {
-        street: undefined;
-        city: undefined;
-        country: undefined;
+        street: undefined | string;
+        city: undefined | string;
+        country: undefined | string;
+        state?: undefined | string;
+        zip?: undefined | string;
     };
     children?: null;
 };
 
-function getMyInfo(): string {
-    const name = "Dmytro";
-    const birthdayDate = new Date("2003-05-13 12:00:00");
-    const numberPhone = "(380) 98-24-72-815";
-    const address = {
+function getMyInfo(UserInfo: UserInfo): string {
+    const { name, birthdayDate, numberPhone, address } = UserInfo;
+    return `Name: ${name}\nBirthday: ${birthdayDate}\nPhone: ${numberPhone}\nAddress: ${JSON.stringify(address)}`;
+}
+
+console.log(getMyInfo({
+    name: "Dmytro",
+    birthdayDate: new Date("2003-05-13 12:00:00"),
+    numberPhone: "(380) 98-24-72-815",
+    address: {
         street: "Chervonoi Kalyny 44",
         city: "Lviv",
         state: "Lvivska",
         zip: "79000",
         country: "Ukraine"
-    };
-
-    return `Name: ${name}\nBirthday: ${birthdayDate}\nPhone: ${numberPhone}\nAddress: ${JSON.stringify(address)}`;
-}
-
-console.log(getMyInfo());
+    }
+}));
