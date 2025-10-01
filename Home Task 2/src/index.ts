539dc774092ba11784933a8592d093b4f6b97080
Flayme <flayme@fedora>
2025-10-01 19:40:02 +0300

    add: Function filter task

--- Home Task 2/src/index.ts
+++ Home Task 2/src/index.ts
@@ -1,53 +1,50 @@
 /*
-    Створіть файл tasks.json, який містить щонайменше 10 об'єктів завдань з такими полями:
-    + id (string або number);
-    + title (string);
-    + description (string);
-    + createdAt (string або Date);
-    + status (наприклад: "todo", "in_progress", "done");
-    + priority (наприклад: "low", "medium", "high");
-    + deadline (string або Date).
-
-    + Визначте тип Task, а також окремі типи Status і Priority, які відображають допустимі значення статусів і пріоритетів. Визначити, які поля мають бути обов'язковими, а які опціональними. Дефолтні значення для полів мають зберігатися у відповідних константах
-
-    Напишіть утиліту, яка буде:
-    + приймати дані з JSON-файлу tasks.json; 
-    + перевіряти, чи відповідає структура об’єктів визначеному типу Task.
-    + для кожного обʼєкту перевіряти наявність поля status і priority. якщо відповідне значення не вказано — надавати дефолтне значення з констант
-
     Створіть функції для:
     + отримання деталей завдання за вказаним id
     - створення нового завдання
     - апдейту деталей завдання
     - видалення завдання
-    - фільтрації завдань за статусом, датою створення та пріоритетом;
+    + фільтрації завдань за статусом, датою створення та пріоритетом;
     - перевірки, чи завершено завдання до дедлайну.
 
-    Усі константи та типи винесіть в окремі файли:
-    + constants.ts — для дефолтних значень;
-    + dto/Task.ts — для типів Task, Status, Priority тощо.
 */
 
 import { z } from 'zod';
 
-import TaskList from './task.json'
+import Json from './task.json'
 import { Task } from './dto/Task'
+import { tr } from 'zod/locales';
 
-const TaskListArray = z.array(Task);
-const resultValid = TaskListArray.safeParse(TaskList);
+const TaskArray = z.array(Task);
+const resultValid = TaskArray.safeParse(Json);
 
 // Перевірка помилок
 if (!resultValid.success){
     console.error(resultValid.error)
 }
 
-const TaskManager = resultValid.data ;
+const TaskList = resultValid.data ;
 
 // Отримання деталей завдання за вказаним id
 function DetailID (id: number){
-    return TaskManager.filter((d) => d.id === id);
+    return TaskList?.filter((d) => d.id === id);
 }
+// console.log(DetailID(1))
 
-console.log(DetailID(1))
+function FilterTask(filter:{
+    status?: string,
+    priority?: string,
+    createdAt?: string,
+}){ return TaskList?.filter(task => {
+        const statusSearch = filter.status ? task.status === filter.status: true;
+        const prioritySearch = filter.priority ? task.priority === filter.priority: true;
+        const createdAtSearch = filter.createdAt ? task.createdAt === filter.createdAt: true;
 
-// Створення нового завдання
+        return statusSearch && prioritySearch && createdAtSearch;
+    })
+}
+console.log(FilterTask({
+    status: 'in_progress',
+    priority: 'high',
+    createdAt: '2025-2-10T16:40:00Z'
+}));
