9cdcc34abe4c4f1609eeb9df6880a67a44d3ff20
Flayme <flaymedesign@gmail.com>
2025-10-02 02:21:50 +0300

    upd: Refresh function 'FilterTask'

--- Home Task 2/src/function/FilterTask.ts
+++ Home Task 2/src/function/FilterTask.ts
@@ -1,9 +1,9 @@
 import { TaskList } from "../index";
 
 export function FilterTask(filter:{
-    status?: string,
-    priority?: string,
-    createdAt?: string,
+    status: string,
+    priority: string,
+    createdAt: string,
 }){ return TaskList?.filter(task => {
         const statusSearch = filter.status ? task.status === filter.status: true;
         const prioritySearch = filter.priority ? task.priority === filter.priority: true;
